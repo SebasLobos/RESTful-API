5638f8eac138cf76f8833951e7af698b360a39d4
Sebastian Lobos <62967960+SebasLobos@users.noreply.github.com>
2020-08-11 11:58:18 -0300

    Add files via upload

---
+++ WikiAPI/app.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require ("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+/////////////// Requests Targetting all articles //////////////////
+
+app.route("/articles")
+.get((req, res)=>{
+
+    Article.find({}, (err, foundArticles)=>{
+        if (!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err)
+        }
+    });
+})
+.post((req, res)=>{
+    
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    })
+    newArticle.save((err)=>{
+        if (!err){
+            res.send("Successfully added a new article.")
+        }else{
+            res.send(err);
+        }
+    });
+})
+.delete((req, res)=>{
+
+        Article.deleteMany(
+            {},
+            (err)=>{
+                if(!err){
+                    res.send("Successfully deleted all articles.");
+                }else{
+                    res.send(err);
+                }
+            }
+        );
+});
+
+/////////////// Requests Targetting a specific article //////////////////
+
+app.route("/articles/:articleTitle")
+
+.get((req, res)=>{
+    Article.findOne(
+        {title: req.params.articleTitle},
+        (err, foundarticle)=>{
+            if (foundarticle){
+                res.send(foundarticle);
+            }else{
+                res.send("No article was founded");
+            }
+        }
+    );
+})
+.put((req, rest)=>{
+    Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        (err)=>{
+            if (!err){
+                res.send("Successfully updated article.");
+            }
+        }  
+    );
+})
+.patch((req, res)=>{
+    Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        (err)=>{
+            if (!err){
+                res.send("Successfully updated article.");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+})
+.delete((req, res)=>{
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        (err)=>{
+            if (!err){
+                res.send("Successfully deleted the corresponding article");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+});
+
+////////////////// App Listen  /////////////////////
+
+app.listen(port,() => {
+    console.log("Server running on port "+ port);
+});
